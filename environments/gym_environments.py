a9ab8283b65772d977c0ccedcf9d46471ee1c87d
Patrick Deubel <patrick.deubel@gmail.com>
2021-04-06 11:23:13 +0200

    Added wrapper for general gym environments

---
+++ environments/gym_environments.py
@@ -0,0 +1,29 @@
+import numpy as np
+import gym
+from gym.spaces.utils import flatdim
+
+from environments.i_environment import IEnvironment, registered_environment_classes
+
+
+class GeneralGymEnvironment(IEnvironment):
+
+    def __init__(self, env_seed: int, configuration: dict):
+        self.configuration = configuration
+        self.env = gym.make(configuration["env_id"])
+        self.env.seed(env_seed)
+
+    def get_number_inputs(self):
+        return flatdim(self.env.observation_space)
+
+    def get_number_outputs(self):
+        return flatdim(self.env.action_space)
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, action: np.ndarray):
+        return self.env.step(action)
+
+
+# TODO: Do this registration via class decorator
+registered_environment_classes["GeneralGymEnv"] = GeneralGymEnvironment
