e07ce6aefbe2e978bf57c3a86881b7a0d3d7ceeb
Patrick Deubel <patrick.deubel@gmail.com>
2021-06-23 16:47:56 +0200

    Finished up the new script to generate the pivot table (without LaTeX yet). This script generates only the pivot table without formatting of the LaTeX table.

--- tools/spreadsheet_tools/generate_pivot_table.py
+++ tools/spreadsheet_tools/generate_pivot_table.py
@@ -4,10 +4,11 @@ import numpy as np
 
 def main():
     experiments_data = pd.read_csv("../../spreadsheets/output.csv")
+    pivot_table_save_location = "../../spreadsheets/pivot_table.csv"
 
     # Filters used for each part of the pivot table (e.g. neural network used is CTRNN, ...)
     # For example 'robotic' is experiment_id==1, brain.type==CTRNN and environment.type==GymMujoco
-    globally_filtered_data = apply_global_filters(experiments_data, "robotics")
+    globally_filtered_data = apply_global_filters(experiments_data, global_filter_type="robotics")
 
     environments = ["Hopper-v2", "HalfCheetah-v2", "Walker2d-v2"]
 
@@ -24,20 +25,31 @@ def main():
     # Defines the order of the columns, i.e. the order in this list resembles the order of the columns
     column_order = ["best_mean", "best_amax", "best_len", "conf.elapsed_time_mean"]
 
-    aggregate_functions_per_index = {
+    # These functions will be used to aggregate per row property, for example when the part of the pivot table is
+    # created for the row prop 'brain.number_neurons' then we want to take the np.max over all the best rewards for
+    # the max column
+    aggregate_functions_per_row_property = {
         "best_mean": np.mean,
         "best_amax": np.max,
         "best_len": np.sum,
         "conf.elapsed_time_mean": np.mean
     }
 
-    aggregate_functions_for_all_indices = {
+    # These functions are used to create the total row at the bottom of the pivot table. They are similar to
+    # 'aggregate_functions_per_row_property' but for example for the column that counts how many experiments have been
+    # done, np.sum would be misleading as this would sum over all row_properties. This would be wrong as this much
+    # experiments have not been done but for each row_property the same amount of experiments have been done (this is
+    # a property of the pivot table). If you don't understand this look at the 'Count' column in the generated pivot
+    # table.
+    aggregate_functions_over_all_row_properties = {
         "best_mean": np.mean,
         "best_amax": np.max,
         "best_len": np.mean,
         "conf.elapsed_time_mean": np.mean
     }
 
+    # This simply defines the rounding precision per column. We want Integers for every column except for the column
+    # with the estimated time, which shall have a precision of one (e.g. an experiment lasted 1,2 hours)
     round_mapper = {}
     type_mapper = {}
     for env in environments:
@@ -50,7 +62,11 @@ def main():
                 round_mapper[(env, col)] = 1
 
     rows = []
-
+    # If some row properties have the wrong format in the final pivot table (e.g. brain.number_neurons is a float in
+    # the table), define here the type it shall be rounded to.
+    # Note that for "brain.set_principle_diagonal_elements_of_W_negative" this does not work as these are correctly
+    # typed here but later when creating the LaTeX from the DataFrame they get messed up, so we have to correct this
+    # when modifying the LaTeX part.
     row_property_types = {
         "brain.number_neurons": int
     }
@@ -66,16 +82,18 @@ def main():
                                            order_of_columns=column_order,
                                            row_property_type=row_prop_type))
 
-    # Create the overall pivot table
+    # Create the overall pivot table by appending them vertically
     pivot_table = pd.concat(rows, keys=row_properties, axis=0)
 
+    # This calculates and then appends the total row to the end of the DataFrame
     pivot_table = add_total_row(pivot_table, environments=environments, row_properties=row_properties,
-                                aggregate_functions_per_index=aggregate_functions_per_index,
-                                aggregate_functions_for_all_indices=aggregate_functions_for_all_indices)
+                                aggregate_functions_per_index=aggregate_functions_per_row_property,
+                                aggregate_functions_for_all_indices=aggregate_functions_over_all_row_properties)
 
+    # This rounds each column to the previously defined precision
     pivot_table = round_pivot_table(pivot_table, round_mapper=round_mapper, type_mapper=type_mapper)
 
-    pivot_table.to_csv("spreadsheets/pivot_table.csv")
+    pivot_table.to_csv(pivot_table_save_location)
 
 
 #############################################################################################
@@ -83,13 +101,13 @@ def main():
 #############################################################################################
 
 
-def apply_global_filters(data: pd.DataFrame, filter: str) -> pd.DataFrame:
-    if filter == "robotics":
+def apply_global_filters(data: pd.DataFrame, global_filter_type: str) -> pd.DataFrame:
+    if global_filter_type == "robotics":
         return _apply_robotics_filters(data)
-    elif filter == "generic_ctrnn":
+    elif global_filter_type == "generic_ctrnn":
         return _apply_generic_ctrnn_filters(data)
     else:
-        raise RuntimeError("Chosen global filter '{}' is not known, please choose another.".format(filter))
+        raise RuntimeError("Chosen global filter '{}' is not known, please choose another.".format(global_filter_type))
 
 
 def _apply_robotics_filters(data: pd.DataFrame) -> pd.DataFrame:
@@ -118,6 +136,10 @@ def create_pivot_table_row(data: pd.DataFrame, row_property: str, environments: 
 
     for env in environments:
         locally_filtered_data = data[data["environment.name"] == env]
+
+        # Note that the values are hardcoded here. This is somewhat restricting but these values are currently the best
+        # one to use here. If this is changed, some lines down the conversion of the elapsed time from seconds to
+        # hours need to be changed as well.
         env_pivot_table = pd.pivot_table(locally_filtered_data,
                                          values=["best", "conf.elapsed_time"],
                                          index=[row_property],
