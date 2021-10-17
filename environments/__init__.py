4c151d1c6477cf2f07915f7e2e52b4c747b440dd
Daniel Z <dzimmer@fzi.de>
2021-10-17 11:36:25 +0200

    Renamed file

--- environments/__init__.py
+++ environments/__init__.py
@@ -3,4 +3,4 @@ from environments.collect_points_rays import CollectPointsRays
 from environments.gym_mujoco import GymMujoco
 from environments.reacher_memory import ReacherMemory
 from environments.gym_environments import GeneralGymEnvironment
-from environments.challenger_ai import ChallengerNeuralNetwork
+from environments.challenger_neural_network import ChallengerNeuralNetwork
