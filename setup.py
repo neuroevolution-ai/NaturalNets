ce0ef4f59d41c72e280a785fc6ef1b1a6df241b6
Daniel Zimmermann <dzimmer@fzi.de>
2021-10-23 12:15:42 +0200

    Changed package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="NaturalNets",
+    name="naturalnets",
     version="0.1.0",
     description="NaturalNets implmented in Python",
     url="https://github.com/neuroevolution-ai/NaturalNets",
