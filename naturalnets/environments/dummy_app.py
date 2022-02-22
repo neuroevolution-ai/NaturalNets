dd32883251f5d990ec5c2217f636beea0944a562
Daniel Zimmermann <dzimmer@fzi.de>
2022-02-22 12:14:58 +0100

    Refactored dummy app

--- naturalnets/environments/dummy_app.py
+++ naturalnets/environments/dummy_app.py
@@ -1,6 +1,7 @@
 from re import I
 import numpy as np
 import attr
+import cv2
 import random
 from naturalnets.environments.i_environment import IEnvironment, registered_environment_classes
 
@@ -9,6 +10,12 @@ from naturalnets.environments.i_environment import IEnvironment, registered_envi
 class DummyAppCfg:
     type: str
     number_time_steps: int
+    screen_width: int
+    screen_height: int
+    number_buttons_horizontal: int
+    number_buttons_vertical: int
+    buttons_size_horizontal: int
+    buttons_size_vertical: int
 
 
 class DummyApp(IEnvironment):
@@ -16,36 +23,43 @@ class DummyApp(IEnvironment):
     def __init__(self, env_seed: int, configuration: dict):
 
         self.config = DummyAppCfg(**configuration)
-
-        self.number_checkboxes_horizontal = 5
-        self.number_checkboxes_vertical = 5
-        self.number_checkboxes = self.number_checkboxes_horizontal * self.number_checkboxes_vertical
+        self.number_buttons = self.config.number_buttons_horizontal * self.config.number_buttons_vertical
 
         # Initialize Cuda Array for positions of GUI elements
-        self.gui_elements_rectangles = np.zeros((self.number_checkboxes, 4))
+        self.gui_elements_rectangles = np.zeros((self.number_buttons, 4), dtype=np.uint32)
 
-        self.checkboxes_size = 80
-        self.checkboxes_grid_size = 80
-        self.checkboxes_border = 0
+        grid_size_horizontal = self.config.screen_width / self.config.number_buttons_horizontal
+        grid_size_vertical = self.config.screen_height / self.config.number_buttons_vertical
+        border_horizontal = (grid_size_horizontal - self.config.buttons_size_horizontal)/2
+        border_vertical = (grid_size_vertical - self.config.buttons_size_vertical)/2
 
         # Place all 8 checkboxes in 4 colums and 2 rows
         n = 0
-        for i in range(self.number_checkboxes_horizontal):
-            for j in range(self.number_checkboxes_vertical):
+        for i in range(self.config.number_buttons_horizontal):
+            for j in range(self.config.number_buttons_vertical):
 
-                x = self.checkboxes_grid_size * i + self.checkboxes_border
-                y = self.checkboxes_grid_size * j + self.checkboxes_border
+                x = grid_size_horizontal * i + border_horizontal
+                y = grid_size_vertical * j + border_vertical
 
-                self.gui_elements_rectangles[n, :] = [x, y, self.checkboxes_size, self.checkboxes_size]
+                self.gui_elements_rectangles[n, :] = [x, y, self.config.buttons_size_horizontal, self.config.buttons_size_vertical]
                 n += 1
 
-        self.gui_elements_states = np.zeros(self.number_checkboxes)
+        self.action_x = 0
+        self.action_y = 0
+
+        self.random_number_x = 0
+        self.random_number_y = 0
+
+        self.click_position_x = 0
+        self.click_position_y = 0
+
+        self.gui_elements_states = np.zeros(self.number_buttons)
         self.gui_elements_states[0] = 1
 
         self.t = 0
 
     def get_number_inputs(self):
-        return self.number_checkboxes
+        return self.number_buttons
 
     def get_number_outputs(self):
         return 4
@@ -55,30 +69,30 @@ class DummyApp(IEnvironment):
 
     def step(self, action: np.ndarray):
 
-        click_position = np.zeros(2)
-
-        # Scale actions to click positions
         random_number1 = action[2] * np.random.normal()
         random_number2 = action[3] * np.random.normal()
-        click_position[0] = int(0.5 * (action[0] + 1.0 + random_number1) * 400.0)
-        click_position[1] = int(0.5 * (action[1] + 1.0 + random_number2) * 400.0)
-    
-        #click_position[0] = random.randrange(1,400)
-        #click_position[1] = random.randrange(1,400)
+        self.click_position_x = int(0.5 * (action[0] + 1.0 + random_number1) * self.config.screen_width)
+        self.click_position_y = int(0.5 * (action[1] + 1.0 + random_number2) * self.config.screen_height)
 
-        #checkbox = rand(1:environments.number_checkboxes)
-        #click_position[threadID] = environments.gui_elements_rectangles[checkbox, threadID] + 10 
+        self.action = action
+
+        #self.click_position_x = random.randint(1, self.config.screen_width)
+        #self.click_position_y = random.randint(1, self.config.screen_height)
+
+        #button = random.randrange(self.number_buttons)
+        #self.click_position_x = self.gui_elements_rectangles[button, 0]
+        #self.click_position_y = self.gui_elements_rectangles[button, 1]
 
         rew = 0.0
 
-        for i in range(1,self.number_checkboxes):
+        for i in range(1, self.number_buttons):
 
             rect_x = self.gui_elements_rectangles[i, 0]
             rect_y = self.gui_elements_rectangles[i, 1]
             width = self.gui_elements_rectangles[i, 2]
             height = self.gui_elements_rectangles[i, 3]
 
-            if self.is_point_in_rect(click_position[0], click_position[1], rect_x, rect_y, width, height):
+            if self.is_point_in_rect(self.click_position_x, self.click_position_y, rect_x, rect_y, width, height):
                 if self.gui_elements_states[i] == 0 and self.gui_elements_states[i-1] == 1:
                     rew += 1.0
                     self.gui_elements_states[i] = 1
@@ -99,7 +113,50 @@ class DummyApp(IEnvironment):
         return self.gui_elements_states
 
     def render(self):
-        pass
+
+        red = (0, 0, 255)
+        green = (0, 255, 0)
+        black = (0, 0, 0)
+        blue = (255, 0, 0)
+        orange = (0, 88, 255)
+
+        # Initialize image with white background
+        image = 255 * np.ones(shape=[self.config.screen_width, self.config.screen_height, 3], dtype=np.uint8)
+
+        # Buttons
+        for i in range(self.number_buttons):
+
+            rect_x = self.gui_elements_rectangles[i, 0]
+            rect_y = self.gui_elements_rectangles[i, 1]
+            width = self.gui_elements_rectangles[i, 2]
+            height = self.gui_elements_rectangles[i, 3]
+
+            point1 = (rect_x, rect_y)
+            point2 = (rect_x + width, rect_y + height)
+
+            if self.gui_elements_states[i] == 0:
+                color = blue
+            else:
+                color = green
+
+            image = cv2.rectangle(image, point1, point2, color, -1)
+            image = cv2.rectangle(image, point1, point2, black, 1)
+
+        # Action position
+        action_position_x = int(0.5 * (self.action[0] + 1.0) * self.config.screen_width)
+        action_position_y = int(0.5 * (self.action[1] + 1.0) * self.config.screen_height)
+        image = cv2.circle(image, (action_position_x, action_position_y), 2, orange, -1)
+
+        # Action distribution as ellipse
+        action_distribution_x = abs(int(0.5 * self.action[2] * self.config.screen_width))
+        action_distribution_y = abs(int(0.5 * self.action[3] * self.config.screen_height))
+        image = cv2.ellipse(image, (action_position_x, action_position_y), (action_distribution_x, action_distribution_y), 0, 0, 360, orange, 1)
+
+        # Click position
+        image = cv2.circle(image, (self.click_position_x, self.click_position_y), 3, red, -1)
+
+        cv2.imshow("Dummy App", cv2.resize(image, (self.config.screen_width, self.config.screen_height)))
+        cv2.waitKey(1)
 
     def is_point_in_rect(self, point_x, point_y, rect_x, rect_y, width, height):
 
--- sample.py
+++ sample.py
@@ -7,7 +7,13 @@ configuration = {
     "maximum_env_seed": 100000,
     "environment": {
         "type": "DummyApp",
-        "number_time_steps": 100
+        "number_time_steps": 100,
+        "screen_width": 400,
+        "screen_height": 400,
+        "number_buttons_horizontal": 5,
+        "number_buttons_vertical": 5,
+        "buttons_size_horizontal": 50,
+        "buttons_size_vertical": 30
     },
     "brain": {
         "type": "LSTMNN",
