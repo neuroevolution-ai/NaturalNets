b797df88816d597ce4c3c941b3f0aba2973cf21a
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2022-10-25 21:46:50 +0200

    Updated monkey tester script
    - Removed stop mode, only use done signal directly from environment (runs each
    episode until done=True)
    - Added WandB logging
    - Removed CLI parameters, uses a JSON configuration now
    - Added results_monkey/ to .gitignore
    
    Signed-off-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- monkey_tester.py
+++ monkey_tester.py
@@ -2,20 +2,53 @@ import json
 import logging
 import multiprocessing as mp
 import os
-import shutil
 import sys
 import time
 from copy import deepcopy
 from datetime import datetime
-from typing import Tuple
+from functools import partial
+from socket import gethostname
+from typing import List, Union
 
 import click
 import numpy as np
+import wandb
+from attrs import asdict, define, field, validators
 
 from naturalnets.environments.i_environment import get_environment_class
 
 RANDOM_CLICK_MONKEY_TYPE = "random-clicks"
 RANDOM_WIDGET_MONKEY_TYPE = "random-widgets"
+
+
+def check_categorical(possible_arguments: List[str], instance, attribute, value):
+    if value not in possible_arguments:
+        raise ValueError(f"'{attribute.name}' must be one of the following values: {*possible_arguments,}, and not "
+                         f"'{value}'")
+
+
+@define(slots=True, auto_attribs=True, frozen=True, kw_only=True)
+class MonkeyTesterCfg:
+    num_monkeys: int = field(validator=[validators.instance_of(int), validators.gt(0)])
+    num_processes: int = field(default=mp.cpu_count(), validator=[validators.instance_of(int), validators.gt(0)])
+
+    monkey_tester_type: str = field(validator=[
+        validators.instance_of(str),
+        partial(check_categorical, [RANDOM_CLICK_MONKEY_TYPE, RANDOM_WIDGET_MONKEY_TYPE])]
+    )
+    random_click_prob: float = field(
+        default=None,
+        validator=validators.optional([validators.instance_of(float), validators.ge(0)])
+    )
+
+    environment: dict
+
+    results_dir: str = field(default="results_monkey/", validator=validators.instance_of(str))
+    store_monkey_tester_data: bool = field(default=False, validator=validators.instance_of(bool))
+
+    wandb_logging: bool = field(default=True, validator=validators.instance_of(bool))
+    wandb_entity: str = field(default="neuroevolution", validator=validators.instance_of(str))
+    wandb_project: str = field(default="NaturalNets", validator=validators.instance_of(str))
 
 
 class RandomMonkeyTester:
@@ -28,114 +61,93 @@ class RandomMonkeyTester:
         return self.rng.uniform(low=-1.0, high=1.0, size=self.action_size).astype(np.float32)
 
 
-def _rollout_one_iteration(env, monkey, reward_sum: float, rewards: list,
-                           actions: list) -> Tuple[float, list, list]:
-    action = monkey.step()
+def _save_monkey_tester_results(configuration: MonkeyTesterCfg, directory: str, monkey_random_seed: int,
+                                time_started: str, time_ended: str, rewards: List, actions: List, reward_sum):
+    os.makedirs(directory, exist_ok=False)
 
-    observation, reward, done, info = env.step(action)
+    monkey_tester_options = deepcopy(asdict(configuration))
+    monkey_tester_options["monkey_random_seed"] = monkey_random_seed
 
-    reward_sum += reward
+    monkey_tester_options["time_started"] = time_started
+    monkey_tester_options["time_ended"] = time_ended
 
-    rewards += [reward]
-    actions += [action]
+    if configuration.store_monkey_tester_data:
+        np.savez(
+            os.path.join(directory, "data.npz"),
+            rewards=np.array(rewards, dtype=np.float32),
+            actions=np.array(actions, dtype=np.float32)
+        )
 
-    return reward_sum, rewards, actions
+    logging.info(f"Monkey Tester finished: Reward {reward_sum} - Started {time_started} - Ended {time_ended} - "
+                 f"Directory {directory}")
 
-
-def _time_mode_rollout(amount: int, env, monkey) -> Tuple[float, list, list]:
-    reward_sum = 0
-    rewards = []
-    actions = []
-
-    start_time = time.time()
-
-    while time.time() < start_time + amount:
-        reward_sum, rewards, actions = _rollout_one_iteration(env, monkey, reward_sum, rewards, actions)
-
-    return reward_sum, rewards, actions
+    with open(os.path.join(directory, "monkey_tester_options.json"), "w", encoding="utf-8") as f:
+        json.dump(monkey_tester_options, f, indent=4)
 
 
-def _iteration_mode_rollout(amount: int, env, monkey) -> Tuple[float, list, list]:
-    reward_sum = 0
-    rewards = []
-    actions = []
+def run_episode(configuration: MonkeyTesterCfg, directory: str, monkey_random_seed: int):
 
-    for i in range(amount):
-        reward_sum, rewards, actions = _rollout_one_iteration(env, monkey, reward_sum, rewards, actions)
+    if configuration.wandb_logging:
+        wandb.init(
+            entity=configuration.wandb_entity,
+            project=configuration.wandb_project,
+            name=f"{gethostname()}/{configuration.monkey_tester_type}/{os.path.basename(directory)}",
+            config=asdict(configuration),
+            tags=["monkey"]
+        )
 
-    return reward_sum, rewards, actions
-
-
-def run_episode(env_config: dict, monkey_type: str, monkey_random_seed: int, amount: int,
-                stop_mode: str) -> Tuple[float, list, list, str, str]:
     time_started = datetime.fromtimestamp(time.time()).strftime("%a, %d %b %Y %H:%M:%S")
+
+    env_config = configuration.environment
     env_id = env_config["type"]
     env = get_environment_class(env_id)(env_config)
-    env.reset()
+    env.reset(env_seed=monkey_random_seed)
 
-    if monkey_type == RANDOM_CLICK_MONKEY_TYPE:
+    if configuration.monkey_tester_type == RANDOM_CLICK_MONKEY_TYPE:
         monkey = RandomMonkeyTester(monkey_random_seed, env.get_number_outputs())
     else:
-        raise NotImplementedError(f"Monkey type {monkey_type} not yet implemented")
+        raise NotImplementedError(f"Monkey type {configuration.monkey_tester_type} not yet implemented")
 
-    if stop_mode == "time":
-        reward_sum, rewards, actions = _time_mode_rollout(amount, env, monkey)
-    else:
-        reward_sum, rewards, actions = _iteration_mode_rollout(amount, env, monkey)
+    reward_sum = 0
+    rewards = []
+    actions = []
+
+    done = False
+
+    while not done:
+        action = monkey.step()
+
+        observation, reward, done, info = env.step(action)
+
+        reward_sum += reward
+        rewards += [reward]
+        actions += [action]
+
+        if configuration.wandb_logging:
+            wandb.log({
+                "rew": reward_sum
+            })
 
     time_ended = datetime.fromtimestamp(time.time()).strftime("%a, %d %b %Y %H:%M:%S")
 
-    return reward_sum, rewards, actions, time_started, time_ended
+    _save_monkey_tester_results(
+        configuration=configuration,
+        directory=directory,
+        monkey_random_seed=monkey_random_seed,
+        time_started=time_started,
+        time_ended=time_ended,
+        rewards=rewards,
+        actions=actions,
+        reward_sum=reward_sum
+    )
+
+    if configuration.wandb_logging:
+        wandb.finish()
 
 
 @click.command()
-@click.option("-t", "--time", "stop_mode", flag_value="time", default=True, show_default=True,
-              help="Use elapsed time in seconds to stop the monkey testing")
-@click.option("-i", "--iterations", "stop_mode", flag_value="iterations", show_default=True,
-              help="Use the number of iterations to stop the monkey testing")
-@click.option("--amount", type=int, required=True,
-              help="Amount on how long the monkey testing shall run (seconds or number of iterations, depending on "
-                   "stop_mode")
-@click.option("-m", "--monkey-type", default=RANDOM_CLICK_MONKEY_TYPE,
-              type=click.Choice([RANDOM_CLICK_MONKEY_TYPE, RANDOM_WIDGET_MONKEY_TYPE]), show_default=True,
-              help="Choose which type of random monkey tester to use")
-@click.option("--random-click-prob", type=float, help="If the random widget monkey tester is chosen, use this to "
-                                                      "define the probability for random clicks")
-@click.option("-c", "--config", type=str, required=True,
-              help="JSON configuration for the environment. Can be directly the config of the environment or a "
-                   "configuration of a successful training run from which the environment's config is automatically "
-                   "extracted. Note that the configured number of timesteps is ignored because of 'stop_mode' and "
-                   "'--amount'.")
-@click.option("-s", "--sequences", type=int, default=1,
-              help="Defines the amount of monkey testers that shall be run. If processes > 1, they run in parallel,"
-                   "otherwise they run sequentially.")
-@click.option("-p", "--processes", "num_processes", type=int, default=mp.cpu_count(),
-              help="The number of processes to use for the monkey testing")
-@click.option("--root-dir", type=str, default="results_monkey/",
-              help="In this directory, subfolders are automatically created based on the current time")
-@click.option("--directory", type=str,
-              help="This directory is directly used to store the result of the monkey testing, instead of"
-                   "automatically generated subfolders as with '--root-dir'")
-def main(stop_mode: str, amount: int, monkey_type: str, random_click_prob: float, config: str,
-         sequences: int, num_processes: int, root_dir: str, directory: str):
-    if directory is not None:
-        main_chosen_directory = directory
-    else:
-        main_chosen_directory = os.path.join(root_dir, monkey_type, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-
-    try:
-        shutil.rmtree(main_chosen_directory)
-    except FileNotFoundError:
-        pass
-
-    concrete_directories = [f"{main_chosen_directory}-{i}" for i in range(sequences)]
-
-    for _dir in concrete_directories:
-        # Test if any directory already exists and raise an error if that is the case. We do not want
-        # to overwrite existing results.
-        if os.path.exists(_dir):
-            raise RuntimeError(f"'{_dir}' already exists, please choose another directory")
-
+@click.option("-c", "--config", type=str, help="Path to a configuration for a monkey tester")
+def main(config: Union[str, dict]):
     logger = logging.getLogger("")
     formatter = logging.Formatter("[%(asctime)s] - %(funcName)s - %(message)s", datefmt="%a, %d %b %Y %H:%M:%S")
 
@@ -144,68 +156,39 @@ def main(stop_mode: str, amount: int, monkey_type: str, random_click_prob: float
     logger.addHandler(sh)
     logger.setLevel(logging.INFO)
 
+    if isinstance(config, str):
+        with open(config, "r") as f:
+            configuration = MonkeyTesterCfg(**json.load(f))
+    else:
+        configuration = MonkeyTesterCfg(**config)
+
+    main_chosen_directory = os.path.join(
+        configuration.results_dir,
+        configuration.monkey_tester_type,
+        datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    )
+
+    concrete_directories = [f"{main_chosen_directory}-{i}" for i in range(configuration.num_monkeys)]
+
+    for _dir in concrete_directories:
+        # Test if any directory already exists and raise an error if that is the case. We do not want
+        # to overwrite existing results.
+        if os.path.exists(_dir):
+            raise RuntimeError(f"'{_dir}' already exists, please choose another directory")
+
+    # Do not seed this, as we want always different seeds for the monkey tester. This RNG is only used to randomly
+    # generate seeds for the monkey tester, that we can save in the config to reproduce later
     rng = np.random.default_rng()
 
-    with open(config, "r") as f:
-        config_data = json.load(f)
-
-    if "environment" in config_data:
-        env_config = config_data["environment"]
-    else:
-        # Otherwise assume config_data is already only the environment config
-        env_config = config_data
-
-    assert env_config["type"] == "DummyApp" or env_config["type"] == "GUIApp", ("Only these two environments are "
-                                                                                "supported in this script")
-
-    if "number_time_steps" in env_config and env_config["number_time_steps"] is not None:
-        logging.warning("The chosen number of timesteps in the environment config are ignored. In this script, "
-                        "the CLI parameter 'amount' overwrites the environment config's timesteps!")
-
-    random_seeds = rng.integers(2**32, size=sequences)
+    random_seeds = rng.integers(2**32, size=configuration.num_monkeys)
 
     results = []
-    with mp.Pool(num_processes) as p:
-        for monkey_seed in random_seeds:
-            results.append(p.apply_async(run_episode, (env_config, monkey_type, monkey_seed, amount, stop_mode)))
+    with mp.Pool(configuration.num_processes) as p:
+        for _dir, monkey_seed in zip(concrete_directories, random_seeds):
+            results.append(p.apply_async(run_episode, (configuration, _dir, int(monkey_seed))))
 
-        results = [r.get() for r in results]
-
-    general_monkey_tester_options = {
-        "environment": {
-            **env_config
-        },
-        "stop-mode": stop_mode,
-        "amount": amount,
-        "monkey-type": monkey_type,
-        "random-click-probability": random_click_prob,
-        "root-dir": root_dir,
-        "explicit-dir": directory
-    }
-
-    for i, (_dir, res, seed) in enumerate(zip(concrete_directories, results, random_seeds)):
-        os.makedirs(_dir, exist_ok=False)
-
-        monkey_tester_options = deepcopy(general_monkey_tester_options)
-        monkey_tester_options["monkey_random_seed"] = int(seed)  # Must convert to standard Python type because of json
-
-        reward_sum, rewards, actions, time_started, time_ended = res
-
-        monkey_tester_options["time_started"] = time_started
-        monkey_tester_options["time_ended"] = time_ended
-
-        current_dir = os.path.join(_dir, "data.npz")
-        np.savez(
-            current_dir,
-            rewards=np.array(rewards, dtype=np.float32),
-            actions=np.array(actions, dtype=np.float32)
-        )
-
-        logging.info(f"Monkey Testing {i}: Reward {reward_sum} - Started {time_started} - Ended {time_ended} - "
-                     "Directory {current_dir}")
-
-        with open(os.path.join(_dir, "monkey_tester_options.json"), "w", encoding="utf-8") as f:
-            json.dump(monkey_tester_options, f, indent=4)
+        # Wait for all processes to finish
+        [r.wait() for r in results]
 
     logging.info("Monkey Testing finished")
 
