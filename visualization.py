cf68f457399b1cbbf8b5f6a48acede50f0021164
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2022-10-26 00:03:26 +0200

    Visualization script: Added possibility to visualize monkey testers
    Signed-off-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- visualization.py
+++ visualization.py
@@ -5,28 +5,50 @@ import time
 import click
 import numpy as np
 
+from monkey_tester.monkey_tester import MonkeyTesterCfg, RandomMonkeyTester
 from naturalnets.brains.i_brain import get_brain_class
 from naturalnets.enhancers.i_enhancer import get_enhancer_class, DummyEnhancer
 from naturalnets.environments.i_environment import get_environment_class
 
 
-@click.command()
-@click.option("-e", "--exp-dir", type=str, required=True,
-              help="Relative path to a directory of a trained experiment. Can include the results/ folder or simply "
-                   "be the directory name inside the results/ folder, i.e. 'results/EXP_DIR' or 'EXP_DIR'")
-@click.option("-d", "--delay", type=float, default=0.005, show_default=True, help="The delay between each step, "
-                                                                                  "useful to slow down the rendering")
-def main(exp_dir: str, delay: float):
-    if "results" in exp_dir:
-        directory = exp_dir
-    else:
-        directory = os.path.join("results", exp_dir)
+def monkey_tester_visualization(monkey_dir: str, lag: float):
 
+    with open(os.path.join(monkey_dir, "monkey_tester_options.json"), "r") as f:
+        monkey_tester_config = MonkeyTesterCfg(**json.load(f))
+
+    with open(os.path.join(monkey_dir, "monkey_tester_details.json"), "r") as f:
+        monkey_tester_details = json.load(f)
+
+    monkey_seed = monkey_tester_details["monkey_random_seed"]
+
+    env_config = monkey_tester_config.environment
+    env_id = env_config["type"]
+    env = get_environment_class(env_id)(env_config)
+    env.reset(env_seed=monkey_seed)
+
+    monkey_tester = RandomMonkeyTester(random_seed=monkey_seed, action_size=env.get_number_outputs())
+
+    total_reward = 0
+    done = False
+
+    while not done:
+        action = monkey_tester.step()
+
+        _, reward, done, _ = env.step(action)
+        total_reward += reward
+
+        env.render()
+        time.sleep(lag)
+
+    print(f"Monkey Tester Seed {monkey_seed}: Reward {total_reward}")
+
+
+def experiment_visualization(exp_dir: str, lag: float):
     # Load configuration file
-    with open(os.path.join(directory, "Configuration.json"), "r") as read_file:
+    with open(os.path.join(exp_dir, "Configuration.json"), "r") as read_file:
         configuration = json.load(read_file)
 
-    individual = np.load(os.path.join(directory, "Best_Genome.npy"), allow_pickle=True)
+    individual = np.load(os.path.join(exp_dir, "Best_Genome.npy"), allow_pickle=True)
 
     environment_class = get_environment_class(configuration["environment"]["type"])
     env = environment_class(configuration=configuration["environment"])
@@ -39,7 +61,7 @@ def main(exp_dir: str, delay: float):
     enhancer = enhancer_class(env.get_number_outputs())
 
     brain_class = get_brain_class(configuration["brain"]["type"])
-    brain_state = brain_class.load_brain_state(os.path.join(directory, "Brain_State.npz"))
+    brain_state = brain_class.load_brain_state(os.path.join(exp_dir, "Brain_State.npz"))
 
     brain = brain_class(input_size=env.get_number_inputs(),
                         output_size=env.get_number_outputs() + enhancer.get_number_outputs(),
@@ -68,7 +90,7 @@ def main(exp_dir: str, delay: float):
             fitness_current += rew
 
             env.render(enhancer_info)
-            time.sleep(delay)
+            time.sleep(lag)
 
         fitness_total += fitness_current
 
@@ -76,6 +98,21 @@ def main(exp_dir: str, delay: float):
 
     print("Reward mean: {:4.2f}".format(fitness_total / number_validation_runs))
     print("Finished")
+
+
+@click.command()
+@click.option("-d", "--directory", type=str, required=True,
+              help="Path to the directory of a trained experiment, or a monkey tester")
+@click.option("-l", "--lag", type=float, default=0.005, show_default=True, help="Amount of seconds the script sleeps, "
+                                                                                "after each step. Useful to slow down "
+                                                                                "the rendering.")
+def main(directory: str, lag: float):
+    # Differentiate if a monkey tester or a trained experiment shall be visualized
+    if os.path.exists(os.path.join(directory, "monkey_tester_options.json")):
+        # directory points to a monkey tester
+        monkey_tester_visualization(monkey_dir=directory, lag=lag)
+    else:
+        experiment_visualization(exp_dir=directory, lag=lag)
 
 
 if __name__ == "__main__":
