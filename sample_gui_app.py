9fa6f8009acd4861ca03f1471447a10611a63788
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2022-11-03 11:01:44 +0100

    Fixed sample_gui_app.py by including missing main-idiom
    Signed-off-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- sample_gui_app.py
+++ sample_gui_app.py
@@ -1,29 +1,35 @@
 from naturalnets.train import train
 
-configuration = {
-    "number_generations": 5000,
-    "number_validation_runs": 50,
-    "number_rounds": 5,
-    "maximum_env_seed": 100000,
-    "global_seed": 0,
-    "environment": {
-        "type": "GUIApp",
-        "number_time_steps": 200,
-        "include_fake_bug": False
-    },
-    "brain": {
-        "type": "LSTM",
-        "hidden_layers": [30],
-        "use_bias": True
-    },
-    "optimizer": {
-        "type": "CmaEsDeap",
-        "population_size": 200,
-        "sigma": 0.5
-    },
-    "enhancer": {
-        "type": "RandomEnhancer"
-    }
-}
 
-train(configuration, results_directory="results")
+def main():
+    configuration = {
+        "number_generations": 5000,
+        "number_validation_runs": 50,
+        "number_rounds": 5,
+        "maximum_env_seed": 100000,
+        "global_seed": 0,
+        "environment": {
+            "type": "GUIApp",
+            "number_time_steps": 200,
+            "include_fake_bug": False
+        },
+        "brain": {
+            "type": "LSTM",
+            "hidden_layers": [30],
+            "use_bias": True
+        },
+        "optimizer": {
+            "type": "CmaEsDeap",
+            "population_size": 200,
+            "sigma": 0.5
+        },
+        "enhancer": {
+            "type": "RandomEnhancer"
+        }
+    }
+
+    train(configuration, results_directory="results", debug=False, w_and_b_log=True)
+
+
+if __name__ == "__main__":
+    main()
