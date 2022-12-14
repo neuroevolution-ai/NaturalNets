4d3741e2ac94fe84a4bd7e7d453c88c1ad6f85ca
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2022-12-14 16:39:05 +0100

    Run interactive script: Rearranged to avoid global variables, use CLI to change arguments
    - CLI better, avoids changing the code to alter the behavior
    
    Signed-off-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- run_app_interactive_mode.py
+++ run_app_interactive_mode.py
@@ -1,83 +1,54 @@
-import jsonlines
 import os
+
+import click
 import cv2
+import jsonlines
 import numpy as np
-from naturalnets.environments.i_environment import get_environment_class
+
 from naturalnets.environments.gui_app.enums import Color
+from naturalnets.environments.i_environment import get_environment_class
 from naturalnets.tools.utils import rescale_values
 
 
-config1 = {
-    "environment": {
-        "type": "GUIApp",
-        "number_time_steps": 200,
-        "include_fake_bug": False
-    }
-}
+def run_interactive(config: dict, save_screenshots: bool, save_state_vector: bool, print_reward: bool):
+    def render(action):
+        image = app._render_image()
 
-config2 = {
-    "environment": {
-        "type": "DummyApp",
-        "number_time_steps": 100,
-        "screen_width": 400,
-        "screen_height": 400,
-        "number_button_columns": 5,
-        "number_button_rows": 5,
-        "button_width": 50,
-        "button_height": 30,
-        "fixed_env_seed": True
-    }
-}
+        if save_screenshots:
+            cv2.imwrite(os.path.join('out', 'screenshot.png'), image)
 
-config = config1
+        if action is not None:
+            # Draw the position of the click as a black circle;
+            # thickness=-1 will fill the circle shape with the specified color
+            cv2.circle(
+                image,
+                (action[0], action[1]), radius=4, color=Color.BLACK.value, thickness=-1
+            )
 
-print_reward = True
-save_screenshots = True
-save_state_vector = True
+        cv2.imshow(window_name, image)
 
+    def click_event(event, x, y, _flags, _params):
+        """Sets action when cv2 mouse-callback is detected, i.e. user has clicked."""
+        if event == cv2.EVENT_LBUTTONDOWN:
+            current_action = np.array([x, y])
+            ob, rew, _, info = app.step(rescale_values(current_action,
+                                                       previous_low=0,
+                                                       previous_high=app.get_screen_size(),
+                                                       new_low=-1,
+                                                       new_high=1))
 
-def render(action):
-    image = app._render_image()
+            if print_reward:
+                print(f"Reward: {rew}")
 
-    if save_screenshots:
-        cv2.imwrite(os.path.join('out', 'screenshot.png'), image)
+            if save_state_vector:
+                states = []
+                for state_info, state in zip(info['states_info'], ob):
+                    states.append("({recursion_depth}) {class_name}: ".format(**state_info) + str(state))
 
-    if action is not None:
-        # Draw the position of the click as a black circle;
-        # thickness=-1 will fill the circle shape with the specified color
-        cv2.circle(
-            image,
-            (action[0], action[1]), radius=4, color=Color.BLACK.value, thickness=-1
-        )
+                with jsonlines.open(os.path.join('out', 'state_vector.jsonl'), 'w') as writer:
+                    writer.write_all(states)
 
-    cv2.imshow(window_name, image)
-
-
-def click_event(event, x, y, _flags, _params):
-    """Sets action when cv2 mouse-callback is detected, i.e. user has clicked."""
-    if event == cv2.EVENT_LBUTTONDOWN:
-        current_action = np.array([x, y])
-        ob, rew, _, info = app.step(rescale_values(current_action,
-                                                   previous_low=0,
-                                                   previous_high=app.get_screen_size(),
-                                                   new_low=-1,
-                                                   new_high=1))
-
-        if print_reward:
-            print(f"Reward: {rew}")
-
-        if save_state_vector:
-            states = []
-            for state_info, state in zip(info['states_info'], ob):
-                states.append("({recursion_depth}) {class_name}: ".format(**state_info) + str(state))
-
-            with jsonlines.open(os.path.join('out', 'state_vector.jsonl'), 'w') as writer:
-                writer.write_all(states)
-
-        render(action=current_action)
-
-
-if __name__ == "__main__":
+            render(action=current_action)
 
     # Get environment class from configuration
     environment_class = get_environment_class(config["environment"]["type"])
@@ -106,3 +77,40 @@ if __name__ == "__main__":
         if key == 27:
             cv2.destroyAllWindows()
             break
+
+
+@click.command()
+@click.option("-c", "--config", "config_id", default=1, type=int, help="Config number (1: GUIApp, else: DummyApp)")
+@click.option("-p/-no-p", "--screenshot/--no-screenshot", "save_screenshots",
+              default=True, type=bool, help="Save screenshots?")
+@click.option("-s/-no-s", "--state/--no-state", "save_state_vector", default=True, type=bool, help="Save state vector?")
+@click.option("-r/-no-r", "--reward/--no-reward", "print_reward", default=True, type=bool, help="Print reward?")
+def main(config_id: int, save_screenshots: bool, save_state_vector: bool, print_reward: bool):
+    if config_id == 1:
+        config = {
+            "environment": {
+                "type": "GUIApp",
+                "number_time_steps": 200,
+                "include_fake_bug": False
+            }
+        }
+    else:
+        config = {
+            "environment": {
+                "type": "DummyApp",
+                "number_time_steps": 100,
+                "screen_width": 400,
+                "screen_height": 400,
+                "number_button_columns": 5,
+                "number_button_rows": 5,
+                "button_width": 50,
+                "button_height": 30,
+                "fixed_env_seed": True
+            }
+        }
+
+    run_interactive(config, save_screenshots, save_state_vector, print_reward)
+
+
+if __name__ == "__main__":
+    main()
