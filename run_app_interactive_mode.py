6aa370ac1f19339c857aadda3537d663030daf0c
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2023-02-14 17:06:42 +0100

    Interactive Script: Disable nearest widget click
    Signed-off-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- run_app_interactive_mode.py
+++ run_app_interactive_mode.py
@@ -97,7 +97,7 @@ def main(config_id: int, save_screenshots: bool, save_state_vector: bool, print_
                 "type": "GUIApp",
                 "number_time_steps": 200,
                 "include_fake_bug": False,
-                "nearest_widget_click": True,
+                "nearest_widget_click": False,
                 "return_clickable_elements": True
             }
         }
