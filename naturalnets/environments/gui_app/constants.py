42df610457410679d105c05b99f0dc94b75cfa96
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2023-03-02 18:32:58 +0100

    GUIApp: prep for replacing relative paths
    - Relative paths do not work when the repo is installed as a package,
    we use importlib.resources as a way to get the path to images etc.,
    in an independent way
    - TODO: replace instances of os.path.join(img_path) with the new utils
    function
    
    Signed-off-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- naturalnets/environments/gui_app/constants.py
+++ naturalnets/environments/gui_app/constants.py
@@ -2,7 +2,7 @@
 
 from naturalnets.environments.gui_app.bounding_box import BoundingBox
 
-IMAGES_PATH: str = "naturalnets/environments/gui_app/img/"
+IMAGES_PATH: str = "naturalnets.environments.gui_app.img"
 
 MAIN_PAGE_AREA_BB = BoundingBox(117, 22, 326, 420)
 SETTINGS_AREA_BB = BoundingBox(3, 1, 422, 367)
