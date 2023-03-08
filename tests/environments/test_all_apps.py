4deb26c48ffb34cf7becd2dc1e7c3fac39efb5f6
VitoVoelker <vito.voelker144@gmail.com>
2023-03-08 16:00:04 +0100

    fixed test

--- tests/environments/test_all_apps.py
+++ tests/environments/test_all_apps.py
@@ -12,18 +12,16 @@ class TestPasslockApp:
         Test if the GUIApp environment returns the correct reward for a series of interactions
         """
         for interaction_sequence in test_coordinates_and_rewards:
-            gui_app = PasslockApp({
+            passlock_app = PasslockApp({
                 "type": "GUIApp",
                 "number_time_steps": len(interaction_sequence),
                 "include_fake_bug": False
             })
-            gui_app.reset()
+            passlock_app.reset()
             for interaction in interaction_sequence:
                 # Recorded interactions are integer coordinates, but all environments require floats between [-1, 1]
-                _, rew, _, _ = gui_app.step(
-                    rescale_values(
-                        interaction[:2], previous_low=0, previous_high=447, new_low=-1, new_high=1)
-                )
+                _, rew, _, _ = passlock_app.step([rescale_values(interaction[0], previous_low=0, previous_high=passlock_app.get_screen_width(), new_high=1, new_low=-1),
+                                                  rescale_values(interaction[1], previous_low=0, previous_high=passlock_app.get_screen_height(), new_high=1, new_low=-1)])
                 # Compare reward coming from the environment with the recorded, ground truth reward
                 assert rew == interaction[2]
 
@@ -35,18 +33,15 @@ class TestAnkiApp():
         Test if the GUIApp environment returns the correct reward for a series of interactions
         """
         for interaction_sequence in test_coordinates_and_rewards:
-            gui_app = AnkiApp({
+            anki_app = AnkiApp({
                 "type": "GUIApp",
-                "number_time_steps": len(interaction_sequence),
-                "include_fake_bug": False
+                "number_time_steps": len(interaction_sequence)
             })
-            gui_app.reset()
+            anki_app.reset()
             for interaction in interaction_sequence:
                 # Recorded interactions are integer coordinates, but all environments require floats between [-1, 1]
-                _, rew, _, _ = gui_app.step(
-                    rescale_values(
-                        interaction[:2], previous_low=0, previous_high=447, new_low=-1, new_high=1)
-                )
+                _, rew, _, _ = anki_app.step([rescale_values(interaction[0], previous_low=0, previous_high=anki_app.get_screen_width(), new_high=1, new_low=-1),
+                                              rescale_values(interaction[1], previous_low=0, previous_high=anki_app.get_screen_height(), new_high=1, new_low=-1)])
                 # Compare reward coming from the environment with the recorded, ground truth reward
                 assert rew == interaction[2]
 
@@ -58,17 +53,22 @@ class TestDummyApp():
         Test if the GUIApp environment returns the correct reward for a series of interactions
         """
         for interaction_sequence in test_coordinates_and_rewards:
-            gui_app = DummyApp({
-                "type": "GUIApp",
+            dummy_app = DummyApp({
+                "type": "DummyApp",
                 "number_time_steps": len(interaction_sequence),
-                "include_fake_bug": False
+                "screen_width": 400,
+                "screen_height": 400,
+                "number_button_columns": 5,
+                "number_button_rows": 5,
+                "button_width": 50,
+                "button_height": 30,
+                "fixed_env_seed": True
             })
-            gui_app.reset()
+            dummy_app.reset()
             for interaction in interaction_sequence:
                 # Recorded interactions are integer coordinates, but all environments require floats between [-1, 1]
-                _, rew, _, _ = gui_app.step(
-                    rescale_values(
-                        interaction[:2], previous_low=0, previous_high=447, new_low=-1, new_high=1)
-                )
+                _, rew, _, _ = dummy_app.step([rescale_values(interaction[0], previous_low=0, previous_high=dummy_app.get_screen_width(), new_high=1, new_low=-1),
+                                              rescale_values(interaction[1], previous_low=0, previous_high=dummy_app.get_screen_height(), new_high=1, new_low=-1)])
+                # Compare reward coming from the environment with the recorded, ground truth reward
                 # Compare reward coming from the environment with the recorded, ground truth reward
                 assert rew == interaction[2]
