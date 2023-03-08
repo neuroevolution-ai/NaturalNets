97b60d8ea1248037e944f39c58c07f1b3ee48723
VitoVoelker <vito.voelker144@gmail.com>
2023-03-08 16:11:39 +0100

    changed the tests

--- tests/environments/test_all_apps.py
+++ tests/environments/test_all_apps.py
@@ -1,74 +1,73 @@
-
 from naturalnets.environments.anki.anki_app import AnkiApp
 from naturalnets.environments.dummy_app.dummy_app import DummyApp
 from naturalnets.environments.passlock_app.passlock_app import PasslockApp
-from naturalnets.tools.utils import rescale_values
 
 
 class TestPasslockApp:
 
-    def test_passlock_app_reward(self, test_coordinates_and_rewards):
+    passlock_app = None
+
+    def test_passlock_object_creation(self):
         """
-        Test if the GUIApp environment returns the correct reward for a series of interactions
+        Test if the PasslockApp is created correctly
         """
-        for interaction_sequence in test_coordinates_and_rewards:
-            passlock_app = PasslockApp({
-                "type": "GUIApp",
-                "number_time_steps": len(interaction_sequence),
-                "include_fake_bug": False
-            })
-            passlock_app.reset()
-            for interaction in interaction_sequence:
-                # Recorded interactions are integer coordinates, but all environments require floats between [-1, 1]
-                _, rew, _, _ = passlock_app.step([rescale_values(interaction[0], previous_low=0, previous_high=passlock_app.get_screen_width(), new_high=1, new_low=-1),
-                                                  rescale_values(interaction[1], previous_low=0, previous_high=passlock_app.get_screen_height(), new_high=1, new_low=-1)])
-                # Compare reward coming from the environment with the recorded, ground truth reward
-                assert rew == interaction[2]
+        self.passlock_app = PasslockApp({
+            "type": "PasslockApp",
+            "number_time_steps": 100,
+            "include_fake_bug": False
+        })
+
+        assert self.passlock_app is not None
+
+    def test_passlock_reset(self):
+        """
+        Test if the PaslockApp resets the click position to 0,0
+        """
+        self.passlock_app.click_position_x = 100
+        self.passlock_app.click_position_y = 100
+        self.passlock_app.reset()
+        assert self.passlock_app.click_position_x == 0
+        assert self.passlock_app.click_position_y == 0
 
 
 class TestAnkiApp():
 
-    def test_anki_app_reward(self, test_coordinates_and_rewards):
+    anki_app = AnkiApp({
+        "type": "AnkiApp",
+        "number_time_steps": 100,
+    })
+
+    def test_anki_reset(self):
         """
-        Test if the GUIApp environment returns the correct reward for a series of interactions
+        Test if the AnkiApp resets the click position to 0,0
         """
-        for interaction_sequence in test_coordinates_and_rewards:
-            anki_app = AnkiApp({
-                "type": "GUIApp",
-                "number_time_steps": len(interaction_sequence)
-            })
-            anki_app.reset()
-            for interaction in interaction_sequence:
-                # Recorded interactions are integer coordinates, but all environments require floats between [-1, 1]
-                _, rew, _, _ = anki_app.step([rescale_values(interaction[0], previous_low=0, previous_high=anki_app.get_screen_width(), new_high=1, new_low=-1),
-                                              rescale_values(interaction[1], previous_low=0, previous_high=anki_app.get_screen_height(), new_high=1, new_low=-1)])
-                # Compare reward coming from the environment with the recorded, ground truth reward
-                assert rew == interaction[2]
+        self.anki_app.click_position_x = 100
+        self.anki_app.click_position_y = 100
+        self.anki_app.reset()
+        assert self.anki_app.click_position_x == 0
+        assert self.anki_app.click_position_y == 0
 
 
 class TestDummyApp():
 
-    def test_dummy_app_reward(self, test_coordinates_and_rewards):
+    dummy_app = DummyApp({
+        "type": "DummyApp",
+        "number_time_steps": 100,
+        "screen_width": 400,
+        "screen_height": 400,
+        "number_button_columns": 5,
+        "number_button_rows": 5,
+        "button_width": 50,
+        "button_height": 30,
+        "fixed_env_seed": True
+    })
+
+    def test_dummy_reset(self):
         """
-        Test if the GUIApp environment returns the correct reward for a series of interactions
+        Test if the DummyApp resets the click position to 0,0
         """
-        for interaction_sequence in test_coordinates_and_rewards:
-            dummy_app = DummyApp({
-                "type": "DummyApp",
-                "number_time_steps": len(interaction_sequence),
-                "screen_width": 400,
-                "screen_height": 400,
-                "number_button_columns": 5,
-                "number_button_rows": 5,
-                "button_width": 50,
-                "button_height": 30,
-                "fixed_env_seed": True
-            })
-            dummy_app.reset()
-            for interaction in interaction_sequence:
-                # Recorded interactions are integer coordinates, but all environments require floats between [-1, 1]
-                _, rew, _, _ = dummy_app.step([rescale_values(interaction[0], previous_low=0, previous_high=dummy_app.get_screen_width(), new_high=1, new_low=-1),
-                                              rescale_values(interaction[1], previous_low=0, previous_high=dummy_app.get_screen_height(), new_high=1, new_low=-1)])
-                # Compare reward coming from the environment with the recorded, ground truth reward
-                # Compare reward coming from the environment with the recorded, ground truth reward
-                assert rew == interaction[2]
+        self.dummy_app.click_position_x = 100
+        self.dummy_app.click_position_y = 100
+        self.dummy_app.reset()
+        assert self.dummy_app.click_position_x == 0
+        assert self.dummy_app.click_position_y == 0
